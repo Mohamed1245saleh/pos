05aae9ab24817cbebf0f1f55eaef50fe30e836fb
Mohamed <mohamed.slaeh15692@gmail.com>
2022-06-05 06:12:50 +0200

    Production12

--- app/Order.php
+++ app/Order.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    protected $guarded = [];
+    protected $fillable = ['created_at' , 'total_price'];
 
     public function client()
     {
