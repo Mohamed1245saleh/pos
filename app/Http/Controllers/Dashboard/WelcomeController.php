0089f07b6f60e498cf6e8faf5aa4a2bde096e5ca
Mohamed <mohamed.slaeh15692@gmail.com>
2022-06-06 18:14:50 +0200

    Production14

--- app/Http/Controllers/Dashboard/WelcomeController.php
+++ app/Http/Controllers/Dashboard/WelcomeController.php
@@ -24,7 +24,7 @@ class WelcomeController extends Controller
             DB::raw('EXTRACT(Year FROM created_at) as year'),
             DB::raw('EXTRACT(Month FROM created_at) as month'),
             DB::raw('SUM(total_price) as sum')
-        )->groupBy('month')->get();
+        )->groupBy('month')->groupBy('Year')->get();
 
         return view('dashboard.welcome', compact('categories_count', 'products_count', 'clients_count', 'users_count', 'sales_data'));
 
